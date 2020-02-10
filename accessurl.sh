adebb6972b60b6100820327fb0aa9819778cf6e8
iyappan-dcis <iyappan@dcis.net>
2020-02-10 18:22:36 +0530

    Update accessurl.sh

--- accessurl.sh
+++ accessurl.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-curl -I http://dcis.net/super.html
+
+url="www.dcis.net"
+status_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}\n' $url)
+date=`date`
+
+if [ $status_code -ne "200" ]
+then
+        echo "status check failed"
+        #You can add further actions here
+fi
